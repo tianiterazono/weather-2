bb318d54a48c52f734d9cc5745028766cd11d168
tianiterazono <78136065+tianiterazono@users.noreply.github.com>
2022-03-09 00:12:52 +0000

    query by city ID

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ app.get("/", function(req, res) {
 app.post("/", function(req, res) {
     
     // takes in the zip from the html form, display in // console. Takes in as string, ex. for zip 02139
-        var zip = String(req.body.zipInput);
-        console.log(req.body.zipInput);
+        var cityID = String(req.body.cityidInput);
+        console.log(req.body.cityidInput);
     
     //build up the URL for the JSON query, API Key is // secret and needs to be obtained by signup 
         const units = "imperial";
-        const apiKey = "67f6b382921c1e89b39b20d4f9556f22";
-        const url = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip +  "&units=" + units + "&APPID=" + apiKey;
+        const apiKey = "feac5a4b6ea4734593fb1442b704df05";
+        const url = "https://api.openweathermap.org/data/2.5/weather?id=" + cityID+  "&units=" + units + "&APPID=" + apiKey;
     
     // this gets the data from Open WeatherPI
     https.get(url, function(response){
@@ -34,12 +34,22 @@ app.post("/", function(req, res) {
             const city = weatherData.name;
             const weatherDescription = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
-            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + 
+            "@2x.png";
+            const humidity = weatherData.main.humidity;
+            const windSpeed = weatherData.wind.speed;
+            const windDir = weatherData.wind.deg;
+            const clouds = weatherData.clouds.all;
             
             // displays the output of the results
-            res.write("<h1> The weather is " + weatherDescription + "<h1>");
-            res.write("<h2>The Temperature in " + city + " " + zip + " is " + temp + " Degrees Fahrenheit<h2>");
-            res.write("<img src=" + imageURL +">");
+            res.write("<h1>Current Weather Report</h1>");
+            res.write("<img src="+ imageURL + ">");
+            res.write("<h2> The weather in " + city + " is " + weatherDescription + ".</h2>");
+            res.write("<h3> Temperature: " + temp + " Degrees Fahrenheit </h3>");
+            res.write("<h3> Humidity: " + humidity + "%</h3>");
+            res.write("<h3> Wind Speed: " + windSpeed + " MPH</h3>");
+            res.write("<h3> Wind Direction: " + windDir + " Degrees</h3>");
+            res.write("<h3> Cloudiness: " + clouds + "%</h3>");
             res.send();
         });
     });
